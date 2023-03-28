613858c72219947f2319b3cb1e68772da39158a2
TotiFlame <santifer4321@gmail.com>
2023-03-28 20:51:48 -0300

    lockboxes 0

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    n = len(boxes)
+    opened_boxes = [0]
+    lock_boxes = []
+    keys = boxes[0]
+    for i in range(1, n):
+            if i in opened_boxes:
+                continue
+            if i in keys:
+                opened_boxes.append(i)
+                keys += boxes[i]
+                keys = list(set(keys))
+            else:
+                lock_boxes.append(i)
+    for j in lock_boxes:
+        if j in keys:
+            opened_boxes.append(j)
+            keys += boxes[j]
+            keys = list(set(keys))
+    if len(opened_boxes) == n:
+        return True
+    else:
+        return False
