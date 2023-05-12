593723e8a0c06031d9621754b6d77cf96a7657f6
TotiFlame <santifer4321@gmail.com>
2023-05-12 15:44:04 -0300

    Task check cycle

--- linked_list_cycle/0-check_cycle.c
+++ linked_list_cycle/0-check_cycle.c
@@ -1,2 +1,23 @@
 #include "lists.h"
 
+int check_cycle(listint_t *list)
+{
+    if (list == NULL)
+    {
+        return 0;
+    }
+
+    listint_t* slow = list;
+    listint_t* fast = list->next;
+
+    while (slow != fast)
+    {
+        if (fast == NULL || fast->next == NULL)
+        {
+            return 0;
+        }
+    slow = slow->next;
+    fast = fast->next->next;
+    }
+    return 1;
+}
