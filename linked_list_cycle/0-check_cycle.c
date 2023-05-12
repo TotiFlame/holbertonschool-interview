2a6fecde65f4a16b6dc04cd8be12e4c261e59052
TotiFlame <santifer4321@gmail.com>
2023-05-12 15:52:30 -0300

    check cycle variable declaration fixed

--- linked_list_cycle/0-check_cycle.c
+++ linked_list_cycle/0-check_cycle.c
@@ -2,13 +2,13 @@
 
 int check_cycle(listint_t *list)
 {
+    listint_t* slow = list;
+    listint_t* fast = list->next;
+ 
     if (list == NULL)
     {
         return 0;
     }
-
-    listint_t* slow = list;
-    listint_t* fast = list->next;
 
     while (slow != fast)
     {
